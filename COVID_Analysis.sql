abbedc1bd2592ed4fcc800cfb7807b2d1da30c8b
Wallace Fang <WallaceFang1@gmail.com>
2021-11-01 13:31:05 -0400

    Created query to break down covid deaths by continent

--- COVID_Analysis.sql
+++ COVID_Analysis.sql
@@ -1,17 +1,30 @@
+--Select *
+--From [Covid19-Data-Analysis]..covid_deaths
+--Where continent is not null
+--order by 3, 4
 
 --Total Cases vs. Total Deaths
 --Shows likelihood of dying from covid contraction in US
-Select location, date,total_cases, total_deaths, ROUND((total_deaths/total_cases)*100, 3) as DeathPercentage
-From [Covid19-Data-Analysis]..covid_deaths
-WHERE location like '%states%'
-order by 1, 2
+--Select location, date,total_cases, total_deaths, ROUND((total_deaths/total_cases)*100, 3) as DeathPercentage
+--From [Covid19-Data-Analysis]..covid_deaths
+--WHERE location like '%states%'
+--order by 1, 2
 
 --Total Cases vs. Population
 --Shows countries highest population infected with covid
 
-Select location, population, MAX(total_cases) as HighestInfected, ROUND(MAX(total_cases)/population*100, 3) as InfectedPercentage
+--Breakdown by Continent
+Select location, MAX(cast(Total_deaths as int)) as TotalDeathCount
 From [Covid19-Data-Analysis]..covid_deaths
 --WHERE location like '%states%'
-Group by location, population
-order by 4 desc
+Where continent is null
+Group by location
+order by TotalDeathCount desc
+
+--Select location, MAX(cast(Total_deaths as int)) as TotalDeathCount
+--From [Covid19-Data-Analysis]..covid_deaths
+----WHERE location like '%states%'
+--Where continent is not null
+--Group by location
+--order by TotalDeathCount desc
 
